52eee84bbad2e87a770d19c8dced26997e1c2400
Kyle Rudderforth <kruddzz@gmail.com>
2020-07-16 21:29:46 -0500

    team.sql setup and working

---
+++ db/team.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS team_DB;
+CREATE DATABASE team_DB;
+
+USE team_DB;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id),
+    PRIMARY KEY (id)
+);
+ 
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NULL,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    PRIMARY KEY (id)
+);
