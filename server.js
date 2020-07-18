d9140c893031a0ebd3e4a8b2900aa9acd8f353d2
Kyle Rudderforth <kruddzz@gmail.com>
2020-07-17 21:13:40 -0500

    viewEmployee() function is working

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
-require("console.table");
+const cTable = require("console.table");
 // const sql = require("./sql");
 
 var connection = mysql.createConnection({
@@ -74,4 +74,27 @@ function firstPrompt() {
           break;
       }
     });
+}
+
+// 1) View / Read all employees
+
+function viewEmployee() {
+  console.log("Viewing employees\n");
+
+  var query =
+    `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
+  FROM employee e
+  LEFT JOIN role r ON e.role_id = r.id
+  LEFT JOIN department d ON d.id = r.department_id
+  LEFT JOIN employee m ON m.id = e.manager_id`
+
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+
+    console.table(res);
+    console.log("Employees viewed!\n");
+
+    firstPrompt();
+  });
+
 }
